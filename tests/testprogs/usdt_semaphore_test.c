24c17b7f746676b76e930fad3b16d02b80f8165b
Hitoshi Murakami <bin101bb@gmail.com>
2025-08-31 18:43:01 +0900

    tests: migrate USDT test programs to use libbpf/usdt
    Replace vendored SystemTap SDT headers with libbpf/usdt library.
    Place usdt.h from libbpf/usdt in libbpf-usdt subdirectory to avoid
    conflicts with existing src/usdt.h.
    
    Signed-off-by: Hitoshi Murakami <bin101bb@gmail.com>

--- tests/testprogs/usdt_semaphore_test.c
+++ tests/testprogs/usdt_semaphore_test.c
@@ -2,23 +2,19 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#define _SDT_HAS_SEMAPHORES 1
-
-#include "sdt.h"
-
-__extension__ unsigned short tracetest_testprobe_semaphore
-    __attribute__((unused)) __attribute__((section(".probes")))
-    __attribute__((visibility("hidden")));
+#include "libbpf-usdt/usdt.h"
 
 static long myclock()
 {
   char buffer[100];
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  sprintf(buffer,
-          "tracetest_testprobe_semaphore: %d\n",
-          tracetest_testprobe_semaphore);
-  DTRACE_PROBE2(tracetest, testprobe, tv.tv_sec, buffer);
+  if (USDT_IS_ACTIVE(tracetest, testprobe)) {
+    snprintf(buffer, sizeof(buffer), "USDT is active\n");
+  } else {
+    snprintf(buffer, sizeof(buffer), "USDT is inactive\n");
+  }
+  USDT_WITH_SEMA(tracetest, testprobe, tv.tv_sec, buffer);
   return tv.tv_sec;
 }
 
